b8c2d1e39a4ba626885723d4dfd3677ef6df7db3
marcuswongjw <marcuswongjw@gmail.com>
2026-07-11 07:03:44 +0800

    chore: drop unused closeLesson import from routing

--- js/routing.js
+++ js/routing.js
@@ -4,7 +4,7 @@ import { state } from './state.js?v=2.0.24';
 import { renderAdminDashboard } from './admin.js?v=2.0.24';
 import { openOnboarding, renderCoursesCatalog } from './catalog.js?v=2.0.24';
 import { renderCurriculumGrid, renderDashboard } from './dashboard.js?v=2.0.24';
-import { closeLesson, saveActiveLessonProgress, startModule } from './lesson.js?v=2.0.28';
+import { saveActiveLessonProgress, startModule } from './lesson.js?v=2.0.28';
 import { updateNetworkView } from './network.js?v=2.0.24';
 import { updateStatsDisplay } from './stats.js?v=2.0.24';
 
